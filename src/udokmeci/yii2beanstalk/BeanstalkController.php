1a0b54d1c690caa766a24a813a3da3435fc9d3e3
Ugur <ugurdokmeci@gmail.com>
2014-12-03 12:05:45 +0200

    Listens all tubes and fix issue #1

--- src/udokmeci/yii2beanstalk/BeanstalkController.php
+++ src/udokmeci/yii2beanstalk/BeanstalkController.php
@@ -11,42 +11,90 @@ class BeanstalkController extends Controller
 	const DELAY="delay";
 	const DELAY_PIRORITY="1000";
 	const DELAY_TIME=5;
+	private $tubeActions=[];
 
+	public function listenTubes(){
+		return [];
+	}
 
+	public function getTubeAction($tube){
+		
+		return isset($this->tubeActions[$tube->tube]) ? $this->tubeActions[$tube->tube] :  false;
+	}
 
+	public function getTubes(){
+		return array_merge(\Yii::$app->beanstalk->listTubes(),$this->listenTubes());
+	}
+
+	public function actionIndex(){
+
+	}
 	public function beforeAction($action)
     {
-		if($action->id !="default") {
-			fwrite(STDOUT, Console::ansiFormat("Listening $action->id tube."."\n", [Console::FG_GREEN]));
+		if($action->id == "index") {
+			
 			try {
-	        	while (true) {
-	        		try {
-
-	        			$bean=\Yii::$app->beanstalk->watch( $action->id );
-	        			if(!$bean){
+				foreach ( $this->getTubes() as $key => $tube) {
+					$methodName = 'action' . str_replace(' ', '', ucwords(implode(' ', explode('-', $tube))));
+					if($this->hasMethod($methodName)){
+						$this->tubeActions[$tube]=$methodName;
+						fwrite(STDOUT, Console::ansiFormat("Listening $tube tube."."\n", [Console::FG_GREEN]));
+						$bean=\Yii::$app->beanstalk->watch( $tube );
+						if(!$bean){
 	            			fwrite(STDERR, Console::ansiFormat("Check beanstalkd!"."\n", [Console::FG_RED]));
 	            			return \Yii::$app->end();
 	        			}
 
-						if($bean){
-							$job=$bean->ignore('default')->reserve();
+					} else {
+						fwrite(STDOUT, Console::ansiFormat("Not Listening $tube tube since there is no action defined. $methodName"."\n", [Console::FG_YELLOW]));
+					}
+				}
 
-							switch (call_user_func_array(array($this, "action".ucfirst($action->id)), ["job"=>$job])) {
-								case self::DELETE:
-									\Yii::$app->beanstalk->delete($job);
-									break;
-								case self::DELAY:
-									\Yii::$app->beanstalk->release($job, self::DELAY_PIRORITY,self::DELAY_TIME);
-									break;
-								
-								default:
-									\Yii::$app->beanstalk->release($job);
-									break;
-							}
+				if(count($this->tubeActions)==0){
+					fwrite(STDERR, Console::ansiFormat("No tube found to listen!"."\n", [Console::FG_RED]));
+					return \Yii::$app->end();
 
+				}
+				
+
+	        	while (true) {
+	        		try {
+	        			
+						$job=$bean->reserve();
+						$methodName=$this->getTubeAction($bean->statsJob($job));
+
+						if(!$methodName){
+							fwrite(STDERR, Console::ansiFormat("No method found for job's tube!"."\n", [Console::FG_RED]));
+							break;
 						}
+
+						switch (
+							call_user_func_array(
+								[
+									$this, $methodName
+									
+								]
+								, 
+								[
+									"job"=>$job
+								]
+							)
+							) {
+							case self::DELETE:
+								\Yii::$app->beanstalk->delete($job);
+								break;
+							case self::DELAY:
+								\Yii::$app->beanstalk->release($job, self::DELAY_PIRORITY,self::DELAY_TIME);
+								break;
+							
+							default:
+								\Yii::$app->beanstalk->release($job);
+								break;
+						}
+
+						
 					} catch (\yii\base\ErrorException $e){
-	            		fwrite(STDERR, Console::ansiFormat($e."\n", [Console::FG_RED]));
+	            		fwrite(STDERR, Console::ansiFormat($e->getMessage()."\n", [Console::FG_RED]));
 					}
 					
 					if(\Yii::$app->beanstalk->sleep)
