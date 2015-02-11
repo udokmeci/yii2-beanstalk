2591de628d09517f4e5174661e91a91057d53063
Ugur <ugurdokmeci@gmail.com>
2015-02-11 13:39:55 +0200

    PHPCS

--- src/udokmeci/yii2beanstalk/Beanstalk.php
+++ src/udokmeci/yii2beanstalk/Beanstalk.php
@@ -2,29 +2,31 @@
 namespace udokmeci\yii2beanstalk;
 use Pheanstalk\Pheanstalk;
 
-class Beanstalk extends \yii\base\Component{
-	public $_beanstalk;
-	public $host="127.0.0.1";
-	public $port=11300;
-	public $connectTimeout=1;
+class Beanstalk extends \yii\base\Component
+{
+    public $_beanstalk;
+    public $host="127.0.0.1";
+    public $port=11300;
+    public $connectTimeout=1;
     public $connected=false;
-	public $sleep=false;
+    public $sleep=false;
     
-	public function init()
+    public function init()
     {
 
-    	try{
-    		$this->_beanstalk=new Pheanstalk($this->host,$this->port,$this->connectTimeout);
-    		$connected=true;
-    	}catch (\Pheanstalk\Exception\ConnectionException $e){
-    		\Yii::error($e);
-    	}
+        try{
+            $this->_beanstalk=new Pheanstalk($this->host, $this->port, $this->connectTimeout);
+            $connected=true;
+        }catch (\Pheanstalk\Exception\ConnectionException $e){
+            \Yii::error($e);
+        }
     }
 
-    private function isJson($string) {
-		json_decode($string);
-		return (json_last_error() == JSON_ERROR_NONE);
-	}
+    private function isJson($string) 
+    {
+        json_decode($string);
+        return (json_last_error() == JSON_ERROR_NONE);
+    }
     /**
      * {@inheritDoc}
      */
@@ -33,17 +35,16 @@ class Beanstalk extends \yii\base\Component{
         $priority = \Pheanstalk\PheanstalkInterface::DEFAULT_PRIORITY,
         $delay = \Pheanstalk\PheanstalkInterface::DEFAULT_DELAY,
         $ttr = \Pheanstalk\PheanstalkInterface::DEFAULT_TTR
-    )
-    {
-    	try{
-	    	if(!is_string($data)){
-	    		$data=json_encode($data);
-	    	}
-	        return $this->_beanstalk->put($data, $priority, $delay, $ttr);   
+    ) {
+        try{
+            if(!is_string($data)) {
+                $data=json_encode($data);
+            }
+            return $this->_beanstalk->put($data, $priority, $delay, $ttr);   
         } catch (\Pheanstalk\Exception\ConnectionException $e){
-    		\Yii::error($e);
-    		return false;
-    	}
+            \Yii::error($e);
+            return false;
+        }
     }
     /**
      * {@inheritDoc}
@@ -54,32 +55,35 @@ class Beanstalk extends \yii\base\Component{
         $priority = \Pheanstalk\PheanstalkInterface::DEFAULT_PRIORITY,
         $delay = \Pheanstalk\PheanstalkInterface::DEFAULT_DELAY,
         $ttr = \Pheanstalk\PheanstalkInterface::DEFAULT_TTR
-    )
-    {
-    	try{
-		    $this->_beanstalk->useTube($tube);
-		    return $this->put($data, $priority, $delay, $ttr);
-		} catch (\Pheanstalk\Exception\ConnectionException $e){
-    		\Yii::error($e);
-    		return false;
-    	}
+    ) {
+        try{
+            $this->_beanstalk->useTube($tube);
+            return $this->put($data, $priority, $delay, $ttr);
+        } catch (\Pheanstalk\Exception\ConnectionException $e){
+            \Yii::error($e);
+            return false;
+        }
     }
 
 
-    public function __call($method, $args) {
+    public function __call($method, $args) 
+    {
 
-    	try{
-	    	$result = call_user_func_array(array($this->_beanstalk, $method), $args);
-	    	if ($result instanceof \Pheanstalk\Pheanstalk) return $this;
-	    	if ($result instanceof \Pheanstalk\Job)
-	    		if($this->isJson($result->getData())) $result=new \Pheanstalk\Job($result->getId(),json_decode($result->getData()));
-	    	return $result;
+        try{
+            $result = call_user_func_array(array($this->_beanstalk, $method), $args);
+            if ($result instanceof \Pheanstalk\Pheanstalk) { return $this; 
+            }
+            if ($result instanceof \Pheanstalk\Job) {
+                if($this->isJson($result->getData())) { $result=new \Pheanstalk\Job($result->getId(), json_decode($result->getData())); 
+                } 
+            }
+            return $result;
 
-    	} catch (\Pheanstalk\Exception\ConnectionException $e){
-    		\Yii::error($e);
-	    	return false;
-	    	
+        } catch (\Pheanstalk\Exception\ConnectionException $e){
+            \Yii::error($e);
+            return false;
+            
 
-    	}
-	}
+        }
+    }
 }
--- src/udokmeci/yii2beanstalk/BeanstalkController.php
+++ src/udokmeci/yii2beanstalk/BeanstalkController.php
@@ -5,159 +5,165 @@ use Yii;
 use yii\console\Controller;
 use yii\helpers\Console;
 
-class BeanstalkController extends Controller {
-	const BURY = "bury";
-	const DELETE = "delete";
-	const DELAY = "delay";
-	const DECAY = "decay";
-	const RELEASE = "release";
-	const NO_ACTION = "noAction";
-	const DELAY_PIRORITY = "1000";
-	const DELAY_TIME = 5;
-	const DELAY_MAX = 3;
-	private $lasttimereconnect=null;
-	private $tubeActions = [];
+class BeanstalkController extends Controller
+{
+    const BURY = "bury";
+    const DELETE = "delete";
+    const DELAY = "delay";
+    const DECAY = "decay";
+    const RELEASE = "release";
+    const NO_ACTION = "noAction";
+    const DELAY_PIRORITY = "1000";
+    const DELAY_TIME = 5;
+    const DELAY_MAX = 3;
+    private $lasttimereconnect=null;
+    private $tubeActions = [];
 
-	public function listenTubes() {
-		return [];
-	}
+    public function listenTubes() 
+    {
+        return [];
+    }
 
-	public function getTubeAction($tube) {
+    public function getTubeAction($tube) 
+    {
 
-		return isset($this->tubeActions[$tube->tube]) ? $this->tubeActions[$tube->tube] : false;
-	}
+        return isset($this->tubeActions[$tube->tube]) ? $this->tubeActions[$tube->tube] : false;
+    }
 
-	public function getTubes() {
-		return array_unique(array_merge((array)\Yii::$app->beanstalk->listTubes(), $this->listenTubes()));
-	}
+    public function getTubes() 
+    {
+        return array_unique(array_merge((array)\Yii::$app->beanstalk->listTubes(), $this->listenTubes()));
+    }
 
-	public function actionIndex() {
+    public function actionIndex() 
+    {
 
-	}
+    }
 
-	public function setDBSessionTimout(){
-		try {
-			$this->mysqlSessionTimeout();
-		} catch (\Exception $e) {
-			Yii::error("DB wait timeout did not succeeded ");
-		}
-		
-	}
+    public function setDBSessionTimout()
+    {
+        try {
+            $this->mysqlSessionTimeout();
+        } catch (\Exception $e) {
+            Yii::error("DB wait timeout did not succeeded ");
+        }
+        
+    }
 
-	public function mysqlSessionTimeout(){
-		try {
-			$command = Yii::$app->db->createCommand('SET @@session.wait_timeout = 31536000');
-			$command->execute();	
-		} catch (\Exception $e) {
-			Yii::error("Mysql session.wait_timeout command did not succeeded ");
-		}
-	}
+    public function mysqlSessionTimeout()
+    {
+        try {
+            $command = Yii::$app->db->createCommand('SET @@session.wait_timeout = 31536000');
+            $command->execute();    
+        } catch (\Exception $e) {
+            Yii::error("Mysql session.wait_timeout command did not succeeded ");
+        }
+    }
 
-	public function beforeAction($action) {
-		if ($action->id == "index") {
-			try {
-				foreach ($this->getTubes() as $key => $tube) {
-					$methodName = 'action' . str_replace(' ', '', ucwords(implode(' ', explode('-', $tube))));
-					if ($this->hasMethod($methodName)) {
-						$this->tubeActions[$tube] = $methodName;
-						fwrite(STDOUT, Console::ansiFormat("Listening $tube tube." . "\n", [Console::FG_GREEN]));
-						$bean = \Yii::$app->beanstalk->watch($tube);
-						if (!$bean) {
-							fwrite(STDERR, Console::ansiFormat("Check beanstalkd!" . "\n", [Console::FG_RED]));
-							return \Yii::$app->end();
-						}
+    public function beforeAction($action) 
+    {
+        if ($action->id == "index") {
+            try {
+                foreach ($this->getTubes() as $key => $tube) {
+                    $methodName = 'action' . str_replace(' ', '', ucwords(implode(' ', explode('-', $tube))));
+                    if ($this->hasMethod($methodName)) {
+                        $this->tubeActions[$tube] = $methodName;
+                        fwrite(STDOUT, Console::ansiFormat("Listening $tube tube." . "\n", [Console::FG_GREEN]));
+                        $bean = \Yii::$app->beanstalk->watch($tube);
+                        if (!$bean) {
+                            fwrite(STDERR, Console::ansiFormat("Check beanstalkd!" . "\n", [Console::FG_RED]));
+                            return \Yii::$app->end();
+                        }
 
-					} else {
-						fwrite(STDOUT, Console::ansiFormat("Not Listening $tube tube since there is no action defined. $methodName" . "\n", [Console::FG_YELLOW]));
-					}
-				}
+                    } else {
+                        fwrite(STDOUT, Console::ansiFormat("Not Listening $tube tube since there is no action defined. $methodName" . "\n", [Console::FG_YELLOW]));
+                    }
+                }
 
-				if (count($this->tubeActions) == 0) {
-					fwrite(STDERR, Console::ansiFormat("No tube found to listen!" . "\n", [Console::FG_RED]));
-					return \Yii::$app->end();
+                if (count($this->tubeActions) == 0) {
+                    fwrite(STDERR, Console::ansiFormat("No tube found to listen!" . "\n", [Console::FG_RED]));
+                    return \Yii::$app->end();
+                }
 
-				}
+                while (true) {
+                    try {
+                        if($this->lasttimereconnect==null) {
+                            $this->lasttimereconnect=time();
+                            $this->setDBSessionTimout();
+                                
+                        }
 
-				while (true) {
-					try {
-						if($this->lasttimereconnect==null){
-							$this->lasttimereconnect=time();
-							$this->setDBSessionTimout();
-								
-						}
+                        if(time()-$this->lasttimereconnect > 60*60) {
+                            Yii::$app->db->close();
+                            Yii::$app->db->open();
+                            Yii::info("Reconnecting to the DB");
+                            $this->setDBSessionTimout();
+                            $this->lasttimereconnect=time();
+                        }
 
-						if(time()-$this->lasttimereconnect > 60*60){
-							Yii::$app->db->close();
-							Yii::$app->db->open();
-							Yii::info("Reconnecting to the DB");
-							$this->setDBSessionTimout();
-							$this->lasttimereconnect=time();
-						}
+                        $job = $bean->reserve();
+                        $jobStats=$bean->statsJob($job);
+                        $methodName = $this->getTubeAction($jobStats);
 
-						$job = $bean->reserve();
-						$jobStats=$bean->statsJob($job);
-						$methodName = $this->getTubeAction($jobStats);
+                        if (!$methodName) {
+                            fwrite(STDERR, Console::ansiFormat("No method found for job's tube!" . "\n", [Console::FG_RED]));
+                            break;
+                        }
 
-						if (!$methodName) {
-							fwrite(STDERR, Console::ansiFormat("No method found for job's tube!" . "\n", [Console::FG_RED]));
-							break;
-						}
+                        switch (
+                        call_user_func_array(
+                            [
+                            $this, $methodName,
 
-						switch (
-								call_user_func_array(
-									[
-										$this, $methodName,
+                            ],
+                            [
+                            "job" => $job,
+                            ]
+                        )
+                        ) {
+                        case self::NO_ACTION:
+                            break;
+                        case self::RELEASE:
+                            \Yii::$app->beanstalk->release($job);
+                            break;
+                        case self::BURY:
+                            \Yii::$app->beanstalk->delete($job);
+                            break;
+                        case self::DECAY:
+                            if($jobStats->delay>=static::DELAY_MAX) {
+                                \Yii::$app->beanstalk->delete($job);
+                                fwrite(STDERR, Console::ansiFormat(Yii::t('udokmeci.beanstalkd', 'Decaying Job Deleted!') . "\n", [Console::FG_RED]));
+                            }
+                            else {
+                                \Yii::$app->beanstalk->release($job, static::DELAY_PIRORITY, static::DELAY_TIME^($jobStats->delay+1)); 
+                            }
+                            break;
+                        case self::DELETE:
+                            \Yii::$app->beanstalk->delete($job);
+                            break;
+                        case self::DELAY:
+                            \Yii::$app->beanstalk->release($job, static::DELAY_PIRORITY, static::DELAY_TIME);
+                            break;
 
-									]
-									,
+                        default:
+                            \Yii::$app->beanstalk->bury($job);
+                            break;
+                        }
 
-									[
-										"job" => $job,
-									]
-								)
-							) {
-							case self::NO_ACTION:
-								break;
-							case self::RELEASE:
-								\Yii::$app->beanstalk->release($job);
-								break;
-							case self::BURY:
-								\Yii::$app->beanstalk->delete($job);
-								break;
-							case self::DECAY:
-								if($jobStats->delay>=static::DELAY_MAX){
-									\Yii::$app->beanstalk->delete($job);
-									fwrite(STDERR, Console::ansiFormat(Yii::t('udokmeci.beanstalkd','Decaying Job Deleted!') . "\n", [Console::FG_RED]));
-								}
-								else
-									\Yii::$app->beanstalk->release($job, static::DELAY_PIRORITY, static::DELAY_TIME^($jobStats->delay+1));
-								break;
-							case self::DELETE:
-								\Yii::$app->beanstalk->delete($job);
-								break;
-							case self::DELAY:
-								\Yii::$app->beanstalk->release($job, static::DELAY_PIRORITY, static::DELAY_TIME);
-								break;
+                    } catch (\yii\base\ErrorException $e) {
+                        fwrite(STDERR, Console::ansiFormat($e->getMessage() . "\n", [Console::FG_RED]));
+                    }
 
-							default:
-								\Yii::$app->beanstalk->bury($job);
-								break;
-						}
+                    if (\Yii::$app->beanstalk->sleep) {
+                        usleep(\Yii::$app->beanstalk->sleep);
+                    }
+                }
+            } catch (\Pheanstalk\Exception\ServerException $e) {
 
-					} catch (\yii\base\ErrorException $e) {
-						fwrite(STDERR, Console::ansiFormat($e->getMessage() . "\n", [Console::FG_RED]));
-					}
+                fwrite(STDERR, Console::ansiFormat($e . "\n", [Console::FG_RED]));
+            }
 
-					if (\Yii::$app->beanstalk->sleep) {
-						usleep(\Yii::$app->beanstalk->sleep);
-					}
-				}
-			} catch (\Pheanstalk\Exception\ServerException $e) {
-
-				fwrite(STDERR, Console::ansiFormat($e . "\n", [Console::FG_RED]));
-			}
-
-		}
-	}
+        }
+    }
 
 }
